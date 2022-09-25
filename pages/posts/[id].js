ca02ab24dcbee7a84fb6ccc49da9d503ad24e056
gw.jang <gw.jang@cardoc.kr>
2022-09-25 17:14:07 +0900

    5-4. Dynamic Routes - Implement getStaticProps

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,17 @@
 import Layout from "../../components/layout";
-import { getAllPostIds } from "../../lib/posts";
+import { getAllPostIds, getPostData } from "../../lib/posts";
 
-export default function Post() {
-  return <Layout>...</Layout>;
+export default function Post({ postData }) {
+  console.log("🚀", postData);
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  );
 }
 
 export async function getStaticPaths() {
@@ -10,5 +19,14 @@ export async function getStaticPaths() {
   return {
     paths,
     fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
   };
 }
